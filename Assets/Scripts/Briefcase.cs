0a01d36d90b1590db67aab2324eee34e90361b40
Kevin Leonardrich <kevinleo080808@gmail.com>
2024-02-12 17:05:03 +0700

    BriefCase

---
+++ Assets/Scripts/Briefcase.cs
@@ -0,0 +1,38 @@
+using BNG;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Briefcase : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int OpenBriefHash = Animator.StringToHash("OpenCase");
+    int CloseBriefHash = Animator.StringToHash("CloseCase");
+
+    [Header("Reference")]
+    [SerializeField] Animator _briefCaseAnim;
+    [SerializeField] Button _briefCaseBtn;
+
+    bool isOpen = false;
+
+    private void Start()
+    {
+        _briefCaseBtn.onButtonDown.AddListener(() =>
+        {
+            TriggerOpenCloseAnim();
+        });
+    }
+    public void TriggerOpenCloseAnim()
+    {
+
+        if (isOpen == false)
+        {
+            _briefCaseAnim.SetTrigger(OpenBriefHash);
+        }
+        else
+        {
+            _briefCaseAnim.SetTrigger(CloseBriefHash);
+        }
+    }
+
+}
